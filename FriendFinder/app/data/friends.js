f3bafb424e327f1f7751c94765e4614fa82c2350
Mel Alvarez <melalvarez@mels-mbp.attlocal.net>
2018-03-07 14:00:02 -0800

    Push homework

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -1,11 +1,31 @@
 // data for array of friends
 var friendsArray = [
     {
-        "name": "",
-        "photo": "",
-        "sex": "",
+        "name": "Candice Swanepoel",
+        "photo": "https://celebritybuzzer.org/wp-content/uploads/2013/02/82fb2__tn_audrina-patridge-30.jpg",
+        "sex": "Female",
         "scores": [
-            
+
         ]
-    }
+    },
+    {
+        "name": "Eva Mendes",
+        "photo": "https://thewondrous.com/wp-content/uploads/2010/05/Eva-Mendes.jpg",
+        "sex": "Female",
+        "scores": [
+
+        ]
+    },{
+        "name": "Jessica Alba",
+        "photo": "https://thewondrous.com/wp-content/uploads/2010/05/Jessica-Alba.jpg",
+        "sex": "Female",
+        "scores": [
+
+        ]
+    },
+    
 ]
+
+
+
+module.exports = friendsArray;
